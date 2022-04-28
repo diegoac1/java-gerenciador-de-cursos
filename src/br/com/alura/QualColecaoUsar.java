6b57c4ca05486328b3723b493bba3855716044cb
Diego <diegoacosta005@gmail.com>
2022-04-28 19:52:35 -0300

    first commit

---
+++ src/br/com/alura/QualColecaoUsar.java
@@ -0,0 +1,12 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.TreeSet;
+
+public class QualColecaoUsar {
+
+	public static void main(String[] args) {
+		Collection<Aluno> alunos = new TreeSet<>();
+		alunos.size();
+	}
+}
---
+++ src/br/com/alura/TestaRecibo.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+import java.util.TreeSet;
+
+public class TestaRecibo {
+
+	public static void main(String[] args) {
+		Recibo rec1 = new Recibo();
+		// atribui valores
+		Recibo rec2 = new Recibo();
+		// atribui valores
+		Recibo rec3 = new Recibo();
+		// atribui valores
+		
+		TreeSet<Recibo> recibo = new TreeSet<>();
+	}
+
+}
